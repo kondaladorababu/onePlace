d96ce1f687fd93d17a061dc4b0b8918d548ab0dd
Dora Kondala <kondaladorababu@gmail.com>
2023-12-24 15:08:05 +0530

    Form Validation Added for new Sprint

--- src/Components/Modals/NewSprintModal.js
+++ src/Components/Modals/NewSprintModal.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import '../Modals/NewSprintModal.css';
 import Modal from './Modal'
 import UIContext from '../../Store/UIContextProvider';
@@ -8,36 +8,75 @@ import Input from '../UI/Input';
 function NewSprintModal() {
     const UICtx = useContext(UIContext);
 
+    const [newSprintData, setNewSprintData] = useState({
+        sprintVersion: '',
+        sprintDuration: '',
+        sprintTeamName: ''
+    });
+
+    const [didEdit, setDidEdit] = useState({
+        sprintVersion: false,
+        sprintDuration: false,
+        sprintTeamName: false,
+    });
+
+    const sprintVersionNotValid = didEdit.sprintVersion && newSprintData.sprintVersion === '';
+    const sprintDurationNotValid = didEdit.sprintDuration && newSprintData.sprintDuration === '';
+    const sprintTeamNameNotValid = didEdit.sprintTeamName && newSprintData.sprintTeamName === '';
+
+    function handleInputChange(e) {
+        const { id, value } = e.target;
+        setNewSprintData(prevData => ({
+            ...prevData,
+            [id]: value,
+        }));
+    }
+
+    function handleInputBlur(e) {
+        const { id } = e.target;
+        setDidEdit(prevEdit => ({
+            ...prevEdit,
+            [id]: true,
+        }));
+    }
+
     const handleSubmit = (e) => {
+        e.preventDefault();
+        UICtx.closeModal();
+    }
+
+    const handleClose = (e) => {
         e.preventDefault();
         UICtx.closeModal();
     }
 
     return (
         <Modal open={UICtx.modalType === 'newSprint'}>
-            <form>
-                <h2 className='new-sprint-heading'>Add a New Sprint</h2>
+            <form >
+                <h3 className='new-sprint-heading'>Add a New Sprint</h3>
 
                 <div className="new-sprint-content">
-                    <div className="new-sprint-name">
-                        <Input label={"New Sprint Version"} type={"text"} id={"name"} />
+                    <div className="new-sprint-version">
+                        <Input label={"New Sprint Version"} type={"text"} id={"sprintVersion"} value={newSprintData.sprintVersion} onChange={handleInputChange} onBlur={handleInputBlur} />
+                        <div className="control-error">{sprintVersionNotValid && <p>Please Enter Sprint Version</p>}</div>
                     </div>
 
                     <div className="new-sprint-duration">
-                        <Input label={"New Sprint Duration"} type={"text"} id={"name"} />
+                        <Input label={"New Sprint Duration"} type={"text"} id={"sprintDuration"} value={newSprintData.sprintDuration} onChange={handleInputChange} onBlur={handleInputBlur} />
+                        <div className="control-error">{sprintDurationNotValid && <p>Please Enter Sprint Version</p>}</div>
                     </div>
 
                     <div className="new-sprint-team-name">
-                        <Input label={"New Sprint Team Name"} type={"text"} id={"name"} />
+                        <Input label={"New Sprint Team Name"} type={"text"} id={"sprintTeamName"} value={newSprintData.sprintTeamName} onChange={handleInputChange} onBlur={handleInputBlur} />
+                        <div className="control-error">{sprintTeamNameNotValid && <p>Please Enter Sprint Version</p>}</div>
                     </div>
                 </div>
 
                 <div className="control-row">
-                    <Button status={'new'} onClick={handleSubmit}>{'Close'}</Button>
-                    <Button status={'completed'}>{'Submit'}</Button>
+                    <Button status={'new'} onClick={handleClose}>{'Close'}</Button>
+                    <Button status={'completed'} onClick={handleSubmit} >{'Submit'}</Button>
                 </div>
             </form>
-
         </Modal >
     )
 }
