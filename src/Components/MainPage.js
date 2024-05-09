99cae3abb76d629beaf2eba6de9196ddd29dc007
Dora <kondaladorababu@gmail.com>
2024-05-09 23:46:43 +0530

    Side Navbar content made smaller

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../Components/MainPage.css';
 import TopNavBar from './NavBars/TopNavBar';
-import TeamBoard from './DashBoard/TeamBoard';
+import TeamBoard from './ResDashboard/TeamBoard';
 
 
 function MainPage() {
